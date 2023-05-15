608e0bed42db1c99c77e140fa3745eb3f320f6f3
Eli <69530035+elisvcodes@users.noreply.github.com>
2023-05-14 21:53:04 -0400

    updated the search to return only none redeemed coupons

--- server/controllers/coupons/search.ts
+++ server/controllers/coupons/search.ts
@@ -5,7 +5,7 @@ const search = async (req: Request, res: Response) => {
   const { brand } = req.params;
   try {
     const couponsData = await prismaClient.coupon.findMany({
-      where: { brand: { title: { contains: brand } } },
+      where: { brand: { title: { contains: brand } }, redeemed: false },
       include: { brand: true },
     });
     res.status(200).json({ message: "Success", coupons: couponsData });
